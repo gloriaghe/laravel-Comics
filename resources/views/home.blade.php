93146040b5ae714d9ae553674c3f788227af28d9
Gloria <gloghe@yahoo.it>
2022-07-12 15:58:51 +0200

    fine main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,5 +22,38 @@
     </button>
 </section>
 
-
+<section id="link">
+    <div id="containerBuy">
+        <div>
+            <a href="#">
+                <img src="images/buy-comics-digital-comics.png" alt="DIGITAL COMICS">
+                <span>DIGITAL COMICS</span>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="images/buy-comics-merchandise.png" alt="DC MERCHANDISE">
+                <span>DC MERCHANDISE</span>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="images/buy-comics-shop-locator.png" alt="SUBSCRIPTION">
+                <span>SUBSCRIPTION</span>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="images/buy-comics-subscriptions.png" alt="COMIC SHOP LOCATOR">
+                <span>COMIC SHOP LOCATOR</span>
+            </a>
+        </div>
+        <div>
+            <a href="#">
+                <img src="images/buy-dc-power-visa.svg" alt="DC POWER VISA">
+                <span>DC POWER VISA</span>
+            </a>
+        </div>
+    </div>
+</section>
 @endsection
