10ea539c2dcc81f3f1619361b2a7c27aff90252b
Gloria <gloghe@yahoo.it>
2022-07-13 16:50:53 +0200

    continuo stile

--- resources/views/paginaInterna.blade.php
+++ resources/views/paginaInterna.blade.php
@@ -16,11 +16,14 @@
             <div id="text">
                 <h1>{{$comic['title']}}</h1>
                 <div id="greenbar">
-                    <div>
-                        <span>U.S. Price: {{$comic['price']}}</span>
-                        <span>AVAILABLE</span>
+                    <div class="green" id="green1">
+                        <div>
+                            <span class="greenlight">U.S. Price: </span>
+                            <span>{{$comic['price']}}</span>
+                        </div>
+                        <span class="greenlight">AVAILABLE</span>
                     </div>
-                    <span>Check Availability</span>
+                    <span class="green" id="green2">Check Availability</span>
                 </div>
                 <p>{{$comic['description']}}</p>
             </div>
@@ -30,33 +33,35 @@
             </div>
         </section>
         <section id="sectionData">
-            <div class="data">
-                <h2>Talent</h2>
-                <div>
-                <span>Art by:</span>
-                @foreach ($comic['artists'] as $item)
-                <span>{{$item}}</span>
-                @endforeach
-                </div>
-                <div>
-                    <span>Written by:</span>
-                    @foreach ($comic['writers'] as $item)
+            <div id="sectionDatabox">
+                <div class="data">
+                    <h2>Talent</h2>
+                    <div>
+                    <span>Art by:</span>
+                    @foreach ($comic['artists'] as $item)
                     <span>{{$item}}</span>
-                    @endforeach            </div>
-            </div>
-            <div class="data">
-                <h2>Specs</h2>
-                <div>
-                    <span>Series:</span>
-                    <span>{{$comic['series']}}</span>
+                    @endforeach
+                    </div>
+                    <div>
+                        <span>Written by:</span>
+                        @foreach ($comic['writers'] as $item)
+                        <span>{{$item}}</span>
+                        @endforeach            </div>
                 </div>
-                <div>
-                    <span>U.S. Price:</span>
-                    <span>{{$comic['price']}}</span>
-                </div>
-                <div>
-                    <span>On Sale Date:</span>
-                    <span>{{$comic['sale_date']}}</span>
+                <div class="data">
+                    <h2>Specs</h2>
+                    <div>
+                        <span>Series:</span>
+                        <span>{{$comic['series']}}</span>
+                    </div>
+                    <div>
+                        <span>U.S. Price:</span>
+                        <span>{{$comic['price']}}</span>
+                    </div>
+                    <div>
+                        <span>On Sale Date:</span>
+                        <span>{{$comic['sale_date']}}</span>
+                    </div>
                 </div>
             </div>
         </section>
